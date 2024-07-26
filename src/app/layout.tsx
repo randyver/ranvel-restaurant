c998c95d3bb3465fa3ccbeb389056a845f65683c
randyver <randyverdian99@gmail.com>
2024-07-26 16:39:53 +0700

    fix: layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,3 @@
-// src/app/layout.tsx
 "use client";
 
 import { SessionProvider } from "next-auth/react";
@@ -7,15 +6,13 @@ import "./globals.css";
 
 export default function RootLayout({
   children,
-  session,
 }: {
   children: React.ReactNode;
-  session: any;
 }) {
   return (
     <html lang="en">
       <body>
-        <SessionProvider session={session}>
+        <SessionProvider>
           <Navbar /> {/* Add Navbar here */}
           <main>{children}</main> {/* Render the main content */}
         </SessionProvider>
