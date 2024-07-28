9b27b412858ebb7ac3f66a8efb8538ffb287a534
randyver <randyverdian99@gmail.com>
2024-07-28 14:19:24 +0700

    fix: little bug

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -29,7 +29,7 @@ export default function Home() {
       </div>
       <div className="flex flex-col">
       <TypewriterEffect words={words} className="text-4xl font-semibold"/>
-        <p className="mt-10 mb-6 xl:text-lg">Indulge in a culinary journey where every dish is crafted with passion and precision, using the finest ingredients to bring you an unforgettable dining experience. Whether you're here for a quick bite or a lavish feast, our warm and inviting atmosphere, combined with our dedication to exceptional service, ensures that every visit is a delightful escape from the ordinary. At Ranvel Restaurant, we don't just serve food; we create memories that linger long after the last bite.</p>
+        <p className="mt-10 mb-6 xl:text-lg">Indulge in a culinary journey where every dish is crafted with passion and precision, using the finest ingredients to bring you an unforgettable dining experience. Whether you&apos;re here for a quick bite or a lavish feast, our warm and inviting atmosphere, combined with our dedication to exceptional service, ensures that every visit is a delightful escape from the ordinary. At Ranvel Restaurant, we don&apos;t just serve food; we create memories that linger long after the last bite.</p>
         <div>
           <Button className="xl:text-lg p-6">Order Food<span className="pi pi-shopping-cart ml-2"></span></Button>
         </div>
