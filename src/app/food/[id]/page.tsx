52554f9268e928b20128918d012807979d1f1344
randyver <randyverdian99@gmail.com>
2024-07-29 17:13:03 +0700

    feat: add validation session

--- src/app/food/[id]/page.tsx
+++ src/app/food/[id]/page.tsx
@@ -1,5 +1,13 @@
 import FoodDetail from "@/components/food-details";
+import { redirect } from "next/navigation";
+import { getServerSession } from "next-auth";
 
 export default async function FoodDetailPage() {
+  const session = await getServerSession();
+
+  if (!session) {
+    redirect('/login');
+  }
+
   return <FoodDetail />;
 }
--- src/app/order/[id]/page.tsx
+++ src/app/order/[id]/page.tsx
@@ -1,5 +1,13 @@
 import OrderDetails from "@/components/order-details";
+import { redirect } from "next/navigation";
+import { getServerSession } from "next-auth";
 
-export default function OrderDetailsPage() {
+export default async function OrderDetailsPage() {
+  const session = await getServerSession();
+
+  if (!session) {
+    redirect('/login');
+  }
+
   return <OrderDetails />;
 }
